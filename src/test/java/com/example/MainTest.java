fd8fd5074d0bb4019ff928e2561dd1e838a71298
Jayaprakash-Paspula <paspulajayaprakash@gmail.com>
2026-03-20 15:56:03 +0530

    modified java code

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(5, Main.add(2, 3));
+    }
+}
